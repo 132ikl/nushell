f4a3b7f3265cd448316ab1ca167e84a905c36f72
132ikl <132@ikl.sh>
2025-07-27 23:52:25 -0400

    Update another literal related test

--- tests/shell/pipeline/commands/external.rs
+++ tests/shell/pipeline/commands/external.rs
@@ -722,7 +722,7 @@ fn external_error_with_backtrace() {
 
 #[test]
 fn sub_external_expression_with_and_op_should_raise_proper_error() {
-    let actual = nu!("(nu --testbin cococo false) and true");
+    let actual = nu!("(nu --testbin cococo '(false)') and true");
     assert!(
         actual
             .err
